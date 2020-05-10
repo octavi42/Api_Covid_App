2a0c1c90ac61afb64141412ba3f9393081043a9f
OcTavi24 <49948824+OcTavi24@users.noreply.github.com>
2020-05-10 14:33:02 +0300

    adding comment for dot env

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv/config');
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log('listening at ' + port);
+    console.log('dot env ' + process.env.DATA_BASE)
 });
 app.use(express.static('public'));
 app.use(express.static('public/Pages/Places/placesPageJS'));
